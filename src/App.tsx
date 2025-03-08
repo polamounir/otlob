abd4e1f45f4a4ff07987140edfb658739d3b8b59
polamounir103 <polamounir103@gmail.com>
2025-03-09 00:03:20 +0200

    add vercel insights

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import AppRoutes from "./routes";
 import NavBar from "./components/shared/NavBar";
 import Footer from "./components/shared/Footer";
 
-
 export default function App() {
   return (
     <Router>
+      
       <NavBar />
       <AppRoutes />
       <Footer />
